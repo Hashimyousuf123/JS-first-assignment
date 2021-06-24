327887e0f4582021fae1d28cc3dc526ec9870e25
Hashim yousuf <hashimyousuf789@gmail.com>
2021-06-24 12:30:05 +0500

    js first assignment

---
+++ app .js
@@ -0,0 +1,74 @@
+// ========== Alert =============
+// alert("Hello World");
+// window.alert("Hello World");
+// console.log("Hello World");
+// document.write("Hello World");
+
+// ========== variable for Numbers =============
+// var a = 5;
+// var b = 10;
+// var c = a+b;
+// alert(c);
+
+// ========== variable for String =============
+// var x = "ali";
+// var y = 10;
+// var z = x + y;
+// alert(z);
+
+// ========== concatenation =============
+// var a = 5;
+// var b = "15";
+// var c = a+b;
+// alert(c)
+
+// ========== Math Operations =============
+// var x = "10";
+// var y = 5;
+// var z = x - y;
+// alert(z);
+
+// ========== Post Increment =============
+// var a = 5;
+// var b = a++;
+// var c = a + b;
+// alert(c);
+
+// ========== Pre Increment =============
+// var a = 5;
+
+// console.log(a, "a");
+
+// var b = ++a;
+
+// ========== Pre Increment =============
+// console.log(b, "b");
+// console.log(a, "a");
+
+// var c = a + b;
+
+// console.log(c, "c");
+
+// var a = 5;
+// var b = 10;
+
+// var c = a++ + --a - ++a + --b - ++b;
+// 5   +   5 -   6 +   9 -   10
+// alert(c);
+
+// var a = 5;
+// var b = 10;
+// var c = "abc";
+
+// var d = a * c;
+// alert(d);
+
+// ========== Prompt =============
+// var userName = prompt("Enter User Name");
+// var age = prompt("Enter Age");
+// var city = prompt("Enter city", "Karachi");
+
+// console.log(userName);
+// console.log(age);
+// console.log(city);
+// alert(userName);
